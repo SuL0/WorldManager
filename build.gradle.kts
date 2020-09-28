4e28befcda4d49be34eb8ad2c343dc27e8a9efc7
SuL <jjw011202@naver.com>
2020-09-29 01:01:52 +0900

    Kotlin STD Lib을 외부로 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/KotlinLoader_S-1.4.10.jar"))
     compileOnly("com.destroystokyo.paper", "paper-api", "1.12.2-R0.1-SNAPSHOT")
     compileOnly("org.spigotmc", "spigot", "1.12.2-R0.1-SNAPSHOT")
     compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/CustomEntity_SuL.jar"))
@@ -35,20 +35,12 @@ spigot {
 
 tasks.compileJava.get().options.encoding = "UTF-8"
 
-val fatJar = task("fatJar", type = Jar::class) {
-    archiveFileName.set("${project.name}_S.jar")
-    destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage"))
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    with(tasks.jar.get() as CopySpec)
-}
-
 tasks {
     compileKotlin.get().kotlinOptions.jvmTarget = "1.8"
     compileTestKotlin.get().kotlinOptions.jvmTarget = "1.8"
 
     jar {
-        dependsOn(fatJar)
-        archiveFileName.set("${project.name}_S-NotFat.jar")
-        destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage/NotFat"))
+        archiveFileName.set("${project.name}_S.jar")
+        destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage"))
     }
 }
